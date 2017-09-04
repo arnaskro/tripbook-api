c1735a6ca70d34a503ea84fef09bda77f5f978c5
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-09-04 17:31:15 +0200

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-server '188.166.94.245', port: 80, roles: [:web, :app, :db], primary: true
+server '188.166.94.245', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:arnaskro/tripbook-api.git'
 set :application,     'tribook-api'
@@ -7,17 +7,18 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
+set :rvm_type, :system
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_tripbook_deploy_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
