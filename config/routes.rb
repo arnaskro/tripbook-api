cd53a5c7482cf0084029eb6226f51e7e68f49606
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-02 02:13:47 +0100

    areas routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
     get 'test_auth' => 'test#members_only'
+    get 'search/:query' => 'areas#search'
+    get 'areas/:type/:id' => 'areas#get', as: :area
   end
 
 end
