9e14ded1e9aa759375d6398ae4a725118d01af25
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-05 04:45:51 +0100

    updated user serializers to include local_id

--- app/serializers/user_list_serializer.rb
+++ app/serializers/user_list_serializer.rb
@@ -13,9 +13,13 @@ end
 
 class UserListItemSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :lastname, :email, :url
-  
-  def url
+  attributes :id, :name, :lastname, :email
+
+  attribute :local_id do
+    object.get_local_id
+  end
+
+  attribute :url do
     v1_user_path(id: object.id)
   end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :lastname, :email, :birthday, :age, :gender, :country_id, :city_id, :active
+  attribute :local_id do
+    object.get_local_id
+  end
 
 end
