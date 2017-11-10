562cd86cb52f4b9f677c0e55c1ea9cac33e500a4
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-10 17:25:11 +0100

    trip controller index

--- app/controllers/v1/trips_controller.rb
+++ app/controllers/v1/trips_controller.rb
@@ -3,31 +3,32 @@ class V1::TripsController < ApiController
   before_action :authenticate_v1_user!, only: [:create, :update, :destroy]
 
   def index
-  
-    # if trip type param is missing return bad request response
-    if params[:trip_type]
-      render json: {error: "Missing trip type!"}, status: 400
-    end
-
-    # (required) Get trips for the trip type
-    trips = Trips.where(type: params[:trip_type])
+    trips = Trip.all
+    # (optional) Get trips for the trip type
+    trips = trips.where(trip_type: params[:trip_type]) if params[:trip_type]
     # (optional) if city_id params was provided
     trips = trips.joins(:cities).where("cities.id = ?", params[:city_id]) if params[:city_id]
     # (optional) if country_id params was provided
-    trips = trips.joins(:countries).where("country.id = ?", params[:country_id]) if params[:country_id]
+    trips = trips.joins(:countries).where("countries.id = ?", params[:country_id]) if params[:country_id]
     # (optional) if local_id params was provided
-    trips = trips.joins(:local).where("local.id = ?", params[:local_id]) if params[:local_id]
+    trips = trips.joins(:local).where("locals.id = ?", params[:local_id]) if params[:local_id]
     # Paginate
-    trips = trips.page(page).per(per_page)
+    trips = trips.page(params[:page] || 1).per(params[:per_page] || 20)
 
     render json: trips
   end
 
   def show
-
+    render json: @trip
   end
 
   def create
+    # Create trip
+    trip = Trip.create(trip_params)
+
+    # If trip is valid, create trip destinations
+    trip.trip_destinations.create(params[:trip][:cities].map{ |city_id| { city_id: city_id } }) if trip.valid?
+
 
   end
 
