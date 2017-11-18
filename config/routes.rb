773f688f69644aeb1041103d39be7f1a4e094fba
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-18 20:38:08 +0100

    removed hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'helloworld' => 'api#hello_world'
 
   namespace :v1 do
+    
     mount_devise_token_auth_for 'User', at: 'auth'
     get 'test_auth' => 'test#members_only'
     # Areas
