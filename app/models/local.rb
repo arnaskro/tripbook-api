67c2dba8cfd7741e050fda8281ab1b6c4dbf7b4b
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-21 11:05:53 +0100

    sorting

--- app/models/local.rb
+++ app/models/local.rb
@@ -3,11 +3,17 @@ class Local < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :city, required: true
   has_one :country, through: :city
-  has_many :trips
+  has_many :trips, through: :user
   has_many :bookings, through: :trips
 
   scope :active, -> { where(active: true) }
   scope :inactive, -> { where(active: false) }
+  scope :most_popular, -> { 
+    joins(:bookings)
+    .group("locals.id")
+    .where('trips.trip_type = 1 OR trips.trip_type = 2')
+    .order('count(bookings.id) DESC NULLS LAST')
+  }
 
   def self.search(query, quantity = nil, fields = ["users.name", "users.lastname", "users.email"])
     list = self.joins(:user).where(fields.map{ |field| "#{field} ILIKE :query" }.join(" OR "), query: "%#{query}%")
